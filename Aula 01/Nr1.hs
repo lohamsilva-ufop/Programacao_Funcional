3cc96fb3df673fbe539140a8364982f04736c364
lohamsilva-ufop <loham.silva@aluno.ufop.edu.br>
2023-07-25 18:46:35 -0300

    25-07-2023

--- Aula 01/Nr1.hs
+++ Aula 01/Nr1.hs
@@ -1,4 +1,9 @@
 --1) implemente uma função que, a partir de 3 numeros inteiros retorne a soma dos quadrados dos 2 maiores numeros.
 
+comp:: Int -> Int -> Int
+comp x y
+    | x > y = x
+    | otherwise = y
+
 quadrado :: Int -> Int -> Int -> Int
-quadrado a b c = a * b
+quadrado x y z = ((comp x y)^2) + ((comp y z)^2)
